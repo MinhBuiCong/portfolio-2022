2423a390d085b0808120c4e927c09f8866f9f27f
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-01-30 02:18:58 +0100

    copy file from desktop to repo

---
+++ js/data/project-data.js
@@ -0,0 +1,88 @@
+export const projects = [
+  {
+    id: 0,
+    name: "TekShop",
+    launch: "December 2021",
+    descriptions:
+      "Tekshop is project that we utilise the knowledge we have learned from Noroff. Here we show our skills in Javascript and how to build a webshop",
+    requirements: [
+      "Make a e-commerce site",
+      "Home- , Product list-, Product-detail- and cart page",
+      "Admin page to create, update and delete products",
+      "Must be responsive on all devices",
+    ],
+    technology: [
+      "Strapi",
+      "Heroku",
+      "PostgresSQL",
+      "JavaScript",
+      "HTML",
+      "SASS",
+      "Bootstrap",
+    ],
+    duration: "November 2021 - December 2021",
+    link: "https://tekshop.netlify.app/",
+  },
+  {
+    id: 1,
+    name: "Pillow Home Decoration",
+    launch: "November 2021",
+    descriptions:
+      "Pillow home decoration is a design focused project where we learn how important images and color need to match to get a better designed website",
+    requirements: [
+      "Interior Design website",
+      "Logo with details of placement, colours, sizes",
+      "Typography and font details",
+      "Layout of the pages in terms of columns and grids",
+      "All graphic/image elements and media with details",
+    ],
+    technology: ["JavaScript", "HTML", "SASS", "Bootstrap"],
+    duration: "October 2021 - November 2021",
+    link: "https://mcb-design-2-ma.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Yay Company",
+    launch: "September 2021",
+    descriptions:
+      "Yay Company is a small project that shows us how powerful it is to use a common CSS framework like bootstrap. Here we got in assignment to copy a prototype",
+    requirements: ["Make the website like the prototype"],
+    technology: ["JavaScript", "HTML", "SASS", "Bootstrap"],
+    duration: "August 2021 - September 2021",
+    link: "https://css-frameworks-ca-minh-bui.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Techblog",
+    launch: "May 2021",
+    descriptions:
+      "TechBlog is our final exam where we show what we have learned throughout the first year in Noroff. Here we designed and developed from start to finish in vanilla JS",
+    requirements: [
+      "Home- , about-, list of blog-, blog post specific- and contact page",
+      "Implement a carousel slider",
+      "Show minimum 10 blog posts, and viewer can click to show more blog posts.",
+      "Modal on picture of the blog in blog specific page.",
+      "Use wordpress as headless CMS",
+      "Minimum 12 blog posts",
+    ],
+    technology: ["Wordpress", "JavaScript", "HTML", "CSS"],
+    duration: "April 2021 - May 2021",
+    link: "https://project-exam-1-minhcongbui-public.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Community Science Museum",
+    launch: "December 2020",
+    descriptions:
+      "Community Science Museum is a family friendly webpage to a museum. Here our demographic is children and adults. We have got in assignment to design and develop a page that will suit this requirements",
+    requirements: [
+      "The site must be have a design that appeals to the target audience.",
+      "The site must have at least 4-6 pages.",
+      "Compositional principles, typography, and color schemes must be considered.",
+      "Your HTML should be semantic and neatly indented.",
+    ],
+    technology: ["JavaScript", "HTML", "CSS"],
+    duration: "November 2020 - December 2020",
+    link: "https://community-science-museum-minh.netlify.app/",
+  },
+];
---
+++ js/data/skills-data.js
@@ -0,0 +1,41 @@
+export const skills = [
+  {
+    name: "React",
+    description:
+      "Know some basic knowledge of React. Had a few assignment involving implementing hooks. Landed my first job using React and GraphQL",
+  },
+  {
+    name: "SASS",
+    description:
+      "Have had a few projects utilising SASS, and will say I am quite used to use it as my main styling language",
+  },
+  {
+    name: "HTML",
+    description:
+      "HTML is a must known discipline and I am used to code with it.",
+  },
+  {
+    name: "CSS",
+    description: "Good at it, but prefer SASS over it.",
+  },
+  {
+    name: "JavaScript",
+    description:
+      "The only programming language I have learned and will say I know mostly the basic use of it.",
+  },
+  {
+    name: "Strapi",
+    description:
+      "Have some experience with this when I had my second semester project. Easy to use and install.",
+  },
+  {
+    name: "Wordpress CMS",
+    description:
+      "The first year in Noroff we learned about content management systems and we learned how to fetch and use wordpress.",
+  },
+  {
+    name: "Tailwind CSS",
+    description:
+      "Use this at work as our main CSS framework library. Not quite used to it yet.",
+  },
+];
